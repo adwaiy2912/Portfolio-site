c40b0a9bc4e5f6d5d01f136fd9a32821ac7a76da
Adwaiy Singh <adwaiy2913@gmail.com>
2024-01-15 22:42:10 +0530

    Typewriter animation added

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// gets header and footer height and adds it to main top and bottom margins
-document.addEventListener('DOMContentLoaded', (e) => {
+// get header height and add it to main top margins
+document.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementById('mainHeader');
     const headerHeight = header.offsetHeight;
-    const footer = document.getElementById('mainFooter');
-    const footerHeight = footer.offsetHeight;
 
     const mainContent = document.getElementById('mainContent');
     mainContent.style.marginTop = headerHeight + 'px';
-    mainContent.style.marginBottom = footerHeight + 'px';
 });
 
-// adds right margin to logo -> aligns title
-document.addEventListener('DOMContentLoaded', (e) => {
+// add right margin to logo -> aligns title
+document.addEventListener('DOMContentLoaded', () => {
     const headerButton = document.querySelector('.header__menu--button');
     const headerLogo = document.querySelector('.header__a');
     const buttonHeight = headerButton.offsetWidth;
@@ -20,16 +17,50 @@ document.addEventListener('DOMContentLoaded', (e) => {
     headerLogo.style.marginRight = (buttonHeight - logoHeight) + 'px';
 });
 
-// adds current year in the footer
-document.addEventListener('DOMContentLoaded', (e) => {
+// typewriter effect to home section content
+document.addEventListener('DOMContentLoaded', () => {
+    const typewriter = document.querySelectorAll(".typewriter");
+    const txtArr = Array.from(typewriter).map((txt) => {
+        const textContent = txt.innerText;
+        txt.innerText = '';
+        return textContent;
+    });
+
+    let iSpeed = 100;
+    let iIdx = 0;
+    let itxtLen = txtArr[0].length;
+    let iTextPos = 0;
+    
+    function typewrite() {
+        typewriter[iIdx].innerHTML = txtArr[iIdx].substring(0, iTextPos) + "|";
+        if (iTextPos++ == itxtLen) {
+            iTextPos = 0;
+            setTimeout(() => {
+                typewriter[iIdx].innerHTML = txtArr[iIdx] + ".";
+                iIdx++;
+                if (iIdx != txtArr.length) {
+                    itxtLen = txtArr[iIdx].length;
+                    setTimeout(typewrite, 500);
+                }
+            }, 200);
+        } else {
+            setTimeout(typewrite, iSpeed);
+        }
+    }
+    typewrite();
+});
+
+// add current year in the footer
+document.addEventListener('DOMContentLoaded', () => {
     const year = document.getElementById("year");
     const thisYear = new Date().getFullYear();
     year.setAttribute("datetime", thisYear);
     year.textContent = thisYear;
 });
 
+// add reCAPTCHA check to form; set default state after submission; forward message to gmail
 const form = document.getElementById("contact__form");
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', () => {
     e.preventDefault();
 
     const captchaResponse = grecaptcha.getResponse();
@@ -56,27 +87,3 @@ form.addEventListener('submit', (e) => {
     })
     .catch((err) => console.log(err));
 });
-
-/*
-document.getElementById("contact__form").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var formData = new FormData(this);
-    fetch("http://127.0.0.1:5500", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => {
-        if (!response.ok) {
-        throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log("Form submission successful:", data);
-    })
-    .catch(error => {
-        console.error("There was a problem with the form submission:", error);
-    });
-});
-*/
