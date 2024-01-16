c13ad1f9e3a851e585d4ee8d304ebce3ad07aa02
Adwaiy Singh <adwaiy2913@gmail.com>
2024-01-17 00:54:36 +0530

    Add styling and complete typewriter animation

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // typewriter effect to home section content
 document.addEventListener('DOMContentLoaded', () => {
-    const typewriter = document.querySelectorAll(".typewriter");
-    const txtArr = Array.from(typewriter).map((txt) => {
+    const typewriter1 = document.querySelectorAll(".home__typewriter1");
+    const txtArr = Array.from(typewriter1).map((txt) => {
         const textContent = txt.innerText;
         txt.innerText = '';
         return textContent;
@@ -30,24 +30,80 @@ document.addEventListener('DOMContentLoaded', () => {
     let iIdx = 0;
     let itxtLen = txtArr[0].length;
     let iTextPos = 0;
+    let delta;
     
-    function typewrite() {
-        typewriter[iIdx].innerHTML = txtArr[iIdx].substring(0, iTextPos) + "|";
-        if (iTextPos++ == itxtLen) {
+    const typewrite = async () => {
+        return new Promise((resolve) => {
+        typewriter1[iIdx].innerHTML = txtArr[iIdx].substring(0, iTextPos) + "|";
+        delta = Math.random() * 250;
+
+        if (iTextPos++ === itxtLen) {
             iTextPos = 0;
             setTimeout(() => {
-                typewriter[iIdx].innerHTML = txtArr[iIdx] + ".";
+                typewriter1[iIdx].innerHTML = txtArr[iIdx] + ".";
                 iIdx++;
-                if (iIdx != txtArr.length) {
+                if (iIdx !== txtArr.length) {
                     itxtLen = txtArr[iIdx].length;
-                    setTimeout(typewrite, 500);
+                    setTimeout(() => { typewrite().then(resolve); }, 500);
+                } else {
+                    resolve();
                 }
-            }, 200);
+            }, 200 + delta);
         } else {
-            setTimeout(typewrite, iSpeed);
+            setTimeout(() => { typewrite().then(resolve); }, iSpeed);
         }
+    });
+    };
+    
+    typewrite().then(() => {
+    function TxtType (ele, txt) {
+        this.ele = ele;
+        this.txt = txt;
+        this.loopNum = 0;
+        this.isDeleting = false;
+        this.write = '';
+        this.tick();
+    };
+    TxtType.prototype.tick = function() {
+        let i = this.loopNum % this.txt.length;
+        let fullTxt = this.txt[i];
+
+        if (this.isDeleting) {
+            this.write = fullTxt.substring(0, this.write.length - 1);
+        } else {
+            this.write = fullTxt.substring(0, this.write.length + 1);
+        }
+        this.ele.innerHTML = '<span class="nowrap">'+this.write+'</span>';
+
+        let that = this;
+        let delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.write === fullTxt) {
+            setTimeout(() => {}, 1000);
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.write === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+        setTimeout(() => { that.tick(); }, delta);
+    };
+
+    const typewriter2 = document.querySelectorAll('.home__typewriter2');
+    const typewriter2Items = document.querySelectorAll('.home__typewriter2--item');
+    const txtArr = Array.from(typewriter2Items).map((ele) => {
+        const textContent = ele.innerText;
+        ele.remove();
+        return textContent;
+    });
+    for (let i = 0; i < typewriter2.length; i++) {
+        new TxtType(typewriter2[i], txtArr);
     }
-    typewrite();
+    });
 });
 
 // add current year in the footer
